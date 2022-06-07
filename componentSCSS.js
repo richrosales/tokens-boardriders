184124f00fbbd00023acc3cfc4638c805d90cb25
Rich Rosales <rich.rosales@boardriders.com>
2022-06-06 17:39:41 -0700

    adding rvca + qs

--- componentSCSS.js
+++ componentSCSS.js
@@ -1,22 +1,21 @@
 const StyleDictionaryPackage = require('style-dictionary');
+const {createArray} = require('./fns');
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
 StyleDictionaryPackage.registerFormat({
-    name: 'css/variables',
-    formatter: function (dictionary, config) {
-      return `${this.selector} {
-        ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
-      }`
-    }
-  });
+  name: 'css/variables',
+  formatter: function (dictionary, config) {
+    return `${this.selector} {\n${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}\n}`
+  }
+});
 
 StyleDictionaryPackage.registerTransform({
     name: 'sizes/px',
     type: 'value',
     matcher: function(prop) {
         // You can be more specific here if you only want 'em' units for font sizes
-        return ["fontSize", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
+        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
     },
     transformer: function(prop) {
         // You can also modify the value here if you want to convert pixels to ems
@@ -29,21 +28,26 @@ function getStyleDictionaryConfig(theme) {
     "source": [
       `tokens/${theme}.json`,
     ],
+    "format": {
+      createArray
+    },
     "platforms": {
       "web": {
         "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
         "buildPath": `output/`,
         "files": [{
-            "destination": `${theme}.css`,
-            "format": "css/variables",
-            "selector": `.${theme}-theme`
-          }]
+          "destination": `${theme}.json`,
+          "format": "createArray"
+        }, {
+          "destination": `${theme}.css`,
+          "format": "css/variables",
+          "selector": `.${theme}-theme`
+        }]
       },
       "scss": {
         "transformGroup": "scss",
-        "buildPath": `output/`,
         "files": [{
-          "destination": `${theme}-map.scss`,
+          "destination": `${theme}-map.css`,
           "format": "scss/map-deep",
           "mapName": "my-tokens"
         }]
@@ -56,7 +60,7 @@ console.log('Build started...');
 
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
-['global', 'rvca', 'qs'].map(function (theme) {
+['global', 'default'].map(function (theme) {
 
     console.log('\n==============================================');
     console.log(`\nProcessing: [${theme}]`);
